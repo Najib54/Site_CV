81c0b645c8a8344ae0287451550db949e9a993e4
Najib <sekkournajib@hotmail.fr>
2021-02-23 17:57:22 +0100

    correction

--- contact.php
+++ contact.php
@@ -1,54 +1,66 @@
-<?php
-    
+ <?php
+
     try
-        {
+    {
         // On se connecte à MySQL
-            $bdd = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'root', 'root');
-        }
-        catch(Exception $e)
-                    {
+        $bdd = new PDO('mysql:host=localhost;dbname=cv;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e)
+    {
         // En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-        }
+            die('Erreur : '.$e->getMessage());
+    }
 
+    if (isset($_POST['connexion']))
+    {
+        $idCo = htmlspecialchars($_POST['identifiantCo']);
+        $mdpCo = sha1($_POST['mdpCo']);
 
-        // si la variable "$_POST" contient des infos alors on les traites
-
-        if (isset($_POST['inscription']))
-        extract($_POST);
-
-        {
-
-            if(!empty($_POST['identifiant']) && !empty($_POST['mdp']) && !empty($_POST['confirm']))
-
-
+        // empty = Si les champs ne sont pas vides
+        if(!empty($idCo) && !empty($mdpCo))
             {
-                echo "ok";
+                $reqUser = $bdd->prepare("SELECT * FROM espace_membre WHERE identifiant = ? && mdp = ? ");
+                $reqUser -> execute(array($idCo, $mdpCo));
+                $userExist = $reqUser -> rowCount();
 
-            }
-            else {
-                echo "non";
+                if ($userExist == 1)
+                {
+                    $erreur = " Vous êtes connectés";
 
+
+                } else {
+                    $erreur = " Identifiant ou mot de passe incorrect.";
+                }
+
+            
+
+
+            } else 
+            {
+                $erreur =  "Tous les champs doivent-être complétés !";
             }
-        }s
+        
+
+    }
+
 ?>
-
 
 
 <html>
     <head>
         <title>Connexion</title>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
 
     </head>
     <body>
- 
-    <div class= "titre" style="text-align: center;"><h1 >Inscription</h1> </div>
-    </br></br></br>
+        <div class= "titre" style="text-align: center;"><h1 >Contact</h1> </div>
+        </br></br></br>
 
+    
 
 <div class = "container">
     <div class = "row">
@@ -56,10 +68,10 @@
             </br>
 
     
-        <form action="message.php" method="POST">
+        <form action="contact.php" method="POST">
             <div class="col-md-6 offset-3">
                 <label class="form-label">Identifiant</label>
-                <input type="text" class="form-control" name="identifiant" >
+                <input type="text" class="form-control" name="identifiantCo" value = "<?php if(isset($idCo)) {echo $idCo;} ?>" >
                
             </div> 
 
@@ -67,31 +79,30 @@
             
             <div class="col-md-6 offset-3">
                 <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-                <input type="password" class="form-control" name="mdp">
+                <input type="password" class="form-control" name="mdpCo">
             </div>
 
-            <br>
-
-            <div class="col-md-6 offset-3">
-                <label class="form-label">Confirmez mot de passe</label>
-                <input type="password" class="form-control" name="confirm">
-            </div>
-
-            <br>
-
-          
-            
+            <br>        
             </br></br>
             
-            <button type="submit" class="btn btn-primary col-md-6 offset-3" name="inscription">Je m'inscris</button>
+            <button type="submit" class="btn btn-primary col-md-6 offset-3" name="connexion">Se connecter.</button>
         </form> 
     </div>
 </div>
+</br></br>
+    
+    <?php
+        if(isset($erreur))
+        {
+            echo '<font color = "red" > ' .'<center> '. $erreur ."</center" ."</font>";
+            
+        }
+    ?>
 
 
-
-    </body>
+</body>
 </html>
+
 
 
 
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <title>CV Najib SEKKOUR</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="css/style.css">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
 
     </head>
@@ -17,6 +17,7 @@
                 <li><a href = "#titrePrez">A propos</li>
                 <li><a href = "#titreParc">Ma formation</li>
                 <li><a href = "#titreComp">Mes compétences</li>
+                <li><a href = "inscription.php">Inscription</li>
                 <li><a href = "contact.php">Contact</li>
              </ul>
         </nav>
@@ -91,9 +92,9 @@
         <h1 id="titreContact">Prise de contact</h1>
 
 
-            <p id="message">Merci de vous rendre sur la page contact pour creer un compte qui vous permettra de me laisser un message.</p>
+            <p id="message">Merci de vous rendre sur la page inscription pour creer un compte qui vous permettra de me laisser un message.</p>
             <br></br>
-            <p id="lien"><a href="contact.php">Cliquez ici pour vous rendre sur la page</a></p>
+            <p id="lien"><a href="inscription.php">Cliquez ici pour vous rendre sur la page</a></p>
         </div>
 
         <div class ="footer"> 
